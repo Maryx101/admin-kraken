c6c8977386076ffa62cbca8f5b70789fde996b77
Maryx101 <sultan828928@gmai.com>
2025-05-05 10:23:15 +0500

    first commit

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import loading from "../assets/images/BK11.webp"
+
+function Loading () {
+    return (
+        <div className="loader">
+            <img src={loading} alt="loading" />
+        </div>
+    );
+}
+
+export default Loading;
---
+++ src/services/axiosinstance.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const a = axios.create({
+    baseURL: 'https://eab7d4822e7b2fcd.mokky.dev'
+})
